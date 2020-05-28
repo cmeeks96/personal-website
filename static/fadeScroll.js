776e0f209917855868ba481ca4ef6430965e4241
theconnormeeks <theconnormeeks@gmail.com>
2020-05-27 23:55:24 -0500

    fade bug fixes - no more fade out

---
+++ static/fadeScroll.js
@@ -0,0 +1,18 @@
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowTop = $(this).scrollTop();
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fadeScroll").each(function() {
+      /* Check the location of each desired element */
+      var objectTop = $(this).offset().top;
+      var objectBottom = $(this).offset().top + $(this).innerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (windowBottom >= objectBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
+      } else { //object goes out of view (scrolling up)
+        // if ($(this).css("opacity")==1) {$(this).fadeTo(1000,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
