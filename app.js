c584dedad793bafbeaf13455dd8123e0c4833fd3
theconnormeeks <theconnormeeks@gmail.com>
2020-11-07 21:19:40 +0000

    dark mode switch

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/404.html'));
-// });
 
 // Start the server
 const PORT = process.env.PORT || 8080;
